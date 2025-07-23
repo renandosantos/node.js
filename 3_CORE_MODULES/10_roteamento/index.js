6d511870bc8e7574846d3a0dfe936f94c3e0cb71
renandosantos <renandosantos.ti@gmail.com>
2025-07-23 11:45:34 -0300

    23/07

---
+++ 3_CORE_MODULES/10_roteamento/index.js
@@ -0,0 +1,34 @@
+const http = require("http")
+const fs = require('fs')
+const url = require("url")
+
+const port = 3000
+
+const server = http.createServer((req, res) => {
+    const q = url.parse(req.url, true)
+    const name = urlInfo.query.name
+
+    if(!name) {
+        fs.readFile('index.html', function(err, data) {
+        res.writeHead(200, { 'content-type': 'text/html' })
+        res.write(data)
+        return res.end()
+    })
+    }else{
+
+        const nameNewLine = name + '\n'
+
+        fs.appendFile("arquivo.txt", nameNewLine, function(err,data) {
+            res.writeHead(302, {
+                location: '/'
+            })
+            return res.end()
+        })
+    }
+
+    
+})
+
+server.listen(port, () => {
+    console.log(`Servidor esta rodando na porta ${port}`)
+})
