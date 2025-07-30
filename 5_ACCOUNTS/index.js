f2748159e9e332acd6d66da04a058e5f71a43228
renan rodrigues dos santos <renandosantos.ti@gmail.com>
2025-07-30 12:34:29 -0300

    hora do almoço

--- 5_ACCOUNTS/index.js
+++ 5_ACCOUNTS/index.js
@@ -24,7 +24,52 @@ function operation() {
 ])
 .then((answers) => {
     const action = answers['action']
-    console.log(action)
+    if(action === 'Criar conta'){
+        createaccount()
+    }
 })
 .catch((err) => console.log(err))
+}
+
+//Criação a conta
+function createaccount() {
+    console.log(chalk.bgGreen.black('Parabéns por escolher o nosso banco!'))
+    console.log(chalk.green('defina as opções da sua conta a seguir'))
+    buildaccount()
+}
+
+function buildaccount() {
+
+    inquirer.prompt([
+        {
+            name: 'accountname',
+            message: 'Digite um nome para a sua conta:',
+        },
+    ])
+    .then((answers) => {
+        const accountname = answers['accountname']
+        console.info(accountname)
+
+        if(!fs.existsSync('accounts')){
+            fs.mkdirSync('accounts')
+        }
+
+        if(fs.existsSync(`accounts/${accountname}.json`)) {
+            console.log(chalk.bgRed.black('Esta conta ja existe, escolha outro nome!'),
+            )
+            buildaccount()
+            return
+        }
+
+        fs.writeFileSync(`accounts/${accountname}.json`,
+            '{"balance": 0}',
+            function(err) {
+            console.log(err)
+            },
+        )
+
+        console.log(chalk.green('Parabéns, sua conta foi criada!'))
+        operation()
+    })
+    .catch((err) => console.log(err))
 }
