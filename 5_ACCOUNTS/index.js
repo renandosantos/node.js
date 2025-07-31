9bd32af611819a2f5c44f46f8d0d3be4e2907349
renan rodrigues dos santos <renandosantos.ti@gmail.com>
2025-07-31 20:31:01 -0300

    31/07

--- 5_ACCOUNTS/index.js
+++ 5_ACCOUNTS/index.js
@@ -1,7 +1,7 @@
 import inquirer from 'inquirer'           // Importa o módulo para entrada de dados no terminal
 import chalk from 'chalk'                 // Importa o módulo para estilizar texto no terminal
 import fs from 'fs'                       // Importa o módulo para manipulação de arquivos
-import { exit } from 'process'
+import { parse } from 'path'
 
 operation()                               // Chama a função principal para iniciar o programa
 
@@ -26,11 +26,11 @@ function operation() {
     if (action === 'Criar conta') {       // Se a escolha for "Criar conta"
       createAccount()                     // Chama a função que inicia o processo de criação
     }else if(action === 'Depositar'){     // Se a escolha for "Depositar"
-      deposit()
+      deposit()                           // Chama a função que inicia o processo de deposito
     }else if(action === 'Consultar Saldo'){     // Se a escolha for "Consultar saldo"
-      consult()
+      consult()                           // Chama a função que inicia o processo de consulta de saldo
     }else if(action === 'Sacar'){      // Se a escolha for "Sacar"
-      sacar()
+      sacar()                           // Chama a função que inicia o processo de sacar
     }else if(action === 'Sair'){       // Se a escolha for "Sair"
       console.log(chalk.bgBlue.black('Obrigado por usar o Acoounts'))
       process.exit()
@@ -78,3 +78,75 @@ function buildAccount() {
 })
   .catch((err) => console.log(err))               // Mostra erro caso algo falhe na criação
 }
+
+//add an amount to user account
+function deposit() {
+
+  inquirer.prompt([                     // Pergunta o nome da conta
+    {
+      name: 'accountname',                 //nome do campo
+      message: 'Qual o nome da sua conta?'   // Mensagem da pergunta
+    }
+  ]).then((answers) => {
+
+    const accountname = answers['accountname']
+    
+    //verificar se a conta existe
+    if(!checkaccount(accountname)){
+      return deposit()
+    }
+    inquirer.prompt([
+    {
+      name: 'amount',
+      message: 'Quanto você deseja depositar?',
+    },
+    ]).then((answers) => {
+
+      const amount = answers['amount']
+
+      //add an amount
+      addamount(accountname, amount)
+      operation()
+    })
+    .catch(err => console.log(err))
+  })
+  .catch(err => console.log(err))
+}
+
+
+function checkaccount(accountname){
+  if(!fs.existsSync(`accounts/${accountname}.json`)) {
+    console.log(chalk.bgRed.black('Esta conta não existe, escolha outro nome!'))
+    return false
+}
+return true
+}
+
+function addamount(accountname, amount) {
+  const accountdata = getaccount(accountname)
+  if(!amount) {
+    console.log(chalk.bgRed.black('Ocorreu um erro, tente novamente mais tarde!'))
+    return deposit()
+  }
+  accountdata.balance = parseFloat(amount) + parseFloat(accountdata.balance)
+
+  fs.writeFileSync(
+  `accounts/${accountname}.json`,
+  JSON.stringify(accountdata),
+  function(err) {
+    console.log(err)
+  },
+)
+  console.log(chalk.green(`Foi depositado o valor de R$${amount} na sua conta`),
+)
+}
+
+function getaccount(accountname) {
+  const accountJSON = fs.readFileSync(`accounts/${accountname}.json`, {
+    encoding: 'utf8',
+    flag: 'r'
+    
+  })
+  return JSON.parse(accountJSON)
+}
+
