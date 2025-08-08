d791d0d1d869d2d23d910d9d29226356b1d7e841
renan rodrigues dos santos <renandosantos.ti@gmail.com>
2025-08-08 16:32:26 -0300

    quase acabando

---
+++ 6_EXPRESS/8_modulo_rotas/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
+const basepath = path.join(__dirname, "templates");
+const usersrouter = require('./users')
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+)
+
+app.use(express.json())
+
+  app.use('/users', usersrouter)
+
+app.get("/", (req, res) => {
+  res.sendFile(`${basepath}/index.html`);
+});
+
+app.listen(port, () => {
+  console.log(`App rodando na porta ${port}`);
+});
