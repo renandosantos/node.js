9796c1f105af81dafd95bb570f11f844c807b0d1
renan rodrigues dos santos <renandosantos.ti@gmail.com>
2025-08-20 14:53:54 -0300

    inserindo dados

---
+++ 9_SEQUELIZE/3_insert/index.js
@@ -0,0 +1,54 @@
+const express = require('express') 
+const {engine} = require('express-handlebars') 
+const conn = require('./db/conn')
+const port = 3000 
+
+const user = require('./models/user')
+
+const app = express() 
+
+app.use(express.urlencoded({
+        extended: true 
+    })
+)
+
+app.use(express.json()) 
+
+app.engine('handlebars', engine()) 
+app.set('view engine', 'handlebars') 
+
+app.use(express.static('public')) 
+
+app.get('/users/create', (req, res) => {
+    res.render('adduser')
+})
+
+app.post('/users/create', async (req, res) => {
+    const name = req.body.name
+    const occupation = req.body.occupation
+    let newsletter = req.body.newsletter
+
+    if(newsletter === 'on'){
+        newsletter = true
+    }else{
+        newsletter = false
+    }
+
+    console.log(req.body)
+
+    await user.create({name, occupation, newsletter})
+
+    res.redirect('/')
+})
+
+app.get('/', (req, res) => { 
+    res.render('home') 
+})
+
+
+conn.sync().then(() => {
+    app.listen(3000, ()=> { 
+    console.log(`APP rodando na porta ${port}`) 
+    })
+})
+.catch((err) => console.log(err))
