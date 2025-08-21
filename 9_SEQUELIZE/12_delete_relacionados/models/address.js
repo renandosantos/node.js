b75ec20ac866f924f594167fbae2a54954040453
renan rodrigues dos santos <renandosantos.ti@gmail.com>
2025-08-21 20:09:10 -0300

    ACABEI

---
+++ 9_SEQUELIZE/12_delete_relacionados/models/address.js
@@ -0,0 +1,27 @@
+const { DataTypes } = require('sequelize')
+
+const db = require('../db/conn')
+
+const user = require('./user')
+
+const address = db.define('address', {
+
+    street: {
+        type: DataTypes.STRING,
+        required: true
+    },
+    number: {
+        type: DataTypes.STRING,
+        required: true
+    },
+    city: {
+        type: DataTypes.STRING,
+        required: true
+    },
+
+})
+
+user.hasMany(address)
+address.belongsTo(user)
+
+module.exports = address
