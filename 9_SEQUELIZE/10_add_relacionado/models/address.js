31adf0788884e1192d94820318a56704de359654
renan rodrigues dos santos <renandosantos.ti@gmail.com>
2025-08-21 15:38:59 -0300

    add relacionado

---
+++ 9_SEQUELIZE/10_add_relacionado/models/address.js
@@ -0,0 +1,26 @@
+const { DataTypes } = require('sequelize')
+
+const db = require('../db/conn')
+
+const user = require('./user')
+
+const address = db.define('address', {
+
+    street: {
+        type: DataTypes.STRING,
+        required: true
+    },
+    Number: {
+        type: DataTypes.STRING,
+        required: true
+    },
+    city: {
+        type: DataTypes.STRING,
+        required: true
+    },
+
+})
+
+address.belongsTo(user)
+
+module.exports = address
