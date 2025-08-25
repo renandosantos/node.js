2480660719a7e7c61f53e8f998e426da83312178
renan rodrigues dos santos <renandosantos.ti@gmail.com>
2025-08-25 14:54:22 -0300

    começando o mvc

---
+++ 10_MVC/1_estrutura/index.js
@@ -0,0 +1,40 @@
+// 1. IMPORTS PRINCIPAIS
+// (Express, Handlebars, Conexão com DB, etc.)
+
+const express = require('express')
+const { engine } = require('express-handlebars')
+const conn = require('./db/conn')
+const port = 3000
+
+// 2. IMPORTAÇÃO DOS MÓDULOS MVC
+// (Models e Rotas)
+
+
+// 3. CONFIGURAÇÕES DO APP
+// (Inicialização do App, formulários, pasta 'public' e View Engine)
+
+const app = express()
+app.use(express.urlencoded({
+        extended: true 
+    })
+)
+app.use(express.json()) 
+app.engine('handlebars', engine())
+app.set('view engine', 'handlebars')
+app.use(express.static('public'))
+
+// 4. ROTAS
+// (O App "usa" as rotas importadas aqui)
+
+
+// 5. SERVIDOR
+// (Conecta ao banco e liga o site)
+conn
+.sync()
+//.sync({force: true}) //apagar tudo do banco
+.then(() => {
+    app.listen(3000, ()=> { 
+    console.log(`APP rodando na porta ${port}`) 
+    })
+})
+.catch((err) => console.log(err))
